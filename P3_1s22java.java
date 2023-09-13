21b8a332eb2be92b0039e739ce8105e12684376d
AlexJaviernot <144191141+AlexJaviernot@users.noreply.github.com>
2023-09-12 19:38:32 -0600

    Add files via upload

---
+++ P3_1s22java.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class P3_1s21{
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String cadena;
+		System.out.println("ingresa tu nombre");
+		cadena = teclado.nextLine();
+		System.out.println("Bienvenido" + cadena + ", a la clase");
+    
+    }
+
+ }  
